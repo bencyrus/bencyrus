6d830f12d657560d7ff7b2d2d224e76b68c06bc5
Ben Cyrus <62337982+bencyrus@users.noreply.github.com>
2023-10-09 19:46:59 +0000

    added nbavbar item

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,30 @@
 export default function Navbar() {
   const navbar = document.createElement("nav");
+
+  // Determine current path
+  const currentPath = window.location.pathname;
+
+  // Helper function to determine active style
+  function getLinkStyle(path) {
+    if (path === "/" && currentPath !== "/") {
+      return "text-decoration: underline; color: gray;";
+    }
+    return currentPath.startsWith(path)
+      ? "text-decoration: underline; color: #242526; font-weight: bold;"
+      : "text-decoration: underline; color: gray;";
+  }
+
   navbar.innerHTML = `
-    <ul style='list-style-type: none;'>
-        <li><a href="/" style='text-decoration: underline; color: black; font-weight: bold;'>Home</a></li>
+    <ul style='list-style-type: none; display: flex; justify-content: left; gap: 20px;'>
+        <li><a href="/" style='${getLinkStyle("/")}'>Home</a></li>
+        <li><a href="/swe/swe.html" style='${getLinkStyle(
+          "/swe"
+        )}'>Software</a></li>
+        <li><a href="/photography/photography.html" style='${getLinkStyle(
+          "/photography"
+        )}'>Photography</a></li>
     </ul>
-    `;
+  `;
 
   return navbar;
 }
