a5af9fad3b01d31c44a394028156ea2766cbe104
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-10-14 11:43:31 -0400

    installed tailwind

--- bencyrus/app/page.tsx
+++ bencyrus/app/page.tsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
 
-const Home = () => {
+export default function HomePage() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="">Home</h1>
       <p>Hello World! This is the Home page</p>
       <p>
         Visit the <Link href="/about">About</Link> page.
       </p>
     </div>
   );
-};
-
-export default Home;
+}
