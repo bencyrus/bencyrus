0031d6804e7f98a26260e2469000248663d99cff
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-10-23 17:59:14 -0400

    added footer and styling to footer

--- bencyrus/app/layout.tsx
+++ bencyrus/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Navbar from "../components/navbar";
 import Sidebar from "../components/sidebar";
+import { LuMail, LuPhone } from "react-icons/lu";
 
 export default function RootLayout({
   children,
@@ -9,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-row">
+      <body className="flex flex-row bg-neutral-900/95 text-white h-svh overflow-hidden">
         <div className="hidden md:block">
           <Sidebar />
         </div>
@@ -17,8 +18,11 @@ export default function RootLayout({
           <div className="md:hidden">
             <Navbar />
           </div>
-          <div className="bg-neutral-900/95 flex-1 text-white">{children}</div>
-          <PageFooter />
+          <div className="flex-1 overflow-y-auto">
+            <div>{children}</div>
+            <hr className="border-neutral-700 mx-[20px]" />
+            <PageFooter />
+          </div>
         </div>
       </body>
     </html>
@@ -26,5 +30,34 @@ export default function RootLayout({
 }
 
 function PageFooter() {
-  return <div>Footer</div>;
+  return (
+    <div className="p-[36px]">
+      <div>
+        <h2 className="text-[22px] font-medium">Want to get in touch?</h2>
+        <p className="text-[14px] text-neutral-400 font-light">
+          I am always excited to chat!
+        </p>
+      </div>
+      <div className="flex justify-between mt-[36px] text-neutral-200 font-light md:w-[510px] flex-col md:flex-row gap-[24px]">
+        <div className="flex flex-row items-center gap-[8px]">
+          <div className="text-[24px] bg-neutral-700/80 rounded-full p-[12px]">
+            <LuMail />
+          </div>
+          <div className="flex flex-col">
+            <span className="text-[12px] text-neutral-400">Email</span>
+            <span>realbencyrus@gmail.com</span>
+          </div>
+        </div>
+        <div className="flex flex-row items-center gap-[8px]">
+          <div className="text-[24px] bg-neutral-700/80 rounded-full p-[12px]">
+            <LuPhone />
+          </div>
+          <div className="flex flex-col">
+            <span className="text-[12px] text-neutral-400">Phone</span>
+            <span>+1 (587) 832-4402</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
