d13e08f573b0008d998fc8e4f9a5fba1b8cad0d7
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-10-25 19:22:05 -0400

    made experience look nicer on mobile

--- bencyrus/app/about/page.tsx
+++ bencyrus/app/about/page.tsx
@@ -113,42 +113,63 @@ function ExperienceCard({
   companyUrl,
 }: ExperienceCardProps) {
   return (
-    <div className="flex gap-[22px]">
-      <div className="md:w-[80px] md:h-[80px] w-[60px] h-[60px]">
-        <Image
-          src={companyUrl}
-          alt={company}
-          width={0}
-          height={0}
-          className="w-full h-full object-contain"
-        />
-      </div>
-      <div className="w-full">
-        <h3 className="text-[18px] font-medium">{title}</h3>
-        <div className="flex flex-col gap-[4px]">
-          <span className="text-[16px] text-neutral-400 mt-[6px]">
-            {company}
-          </span>
-          <span className="text-[14px] text-neutral-400">
-            {startDate} - {endDate}
-          </span>
+    <div>
+      <div className="flex gap-[22px]">
+        <div className="md:w-[80px] md:h-[80px] w-[60px] h-[60px]">
+          <Image
+            src={companyUrl}
+            alt={company}
+            width={0}
+            height={0}
+            className="w-full h-full object-contain"
+          />
         </div>
-        <ul className="text-[16px] text-neutral-100 flex flex-col gap-[12px] mt-[28px] list-disc pl-[40px] font-light">
-          {descriptionItems.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-        <div className="flex flex-wrap gap-[12px] mt-[28px]">
-          {skills.map((skill) => (
-            <span
-              key={skill}
-              className="text-[16px] text-neutral-100 font-bold"
-            >
-              {skill}
+        <div className="w-full">
+          <h3 className="text-[18px] font-medium">{title}</h3>
+          <div className="flex flex-col gap-[4px]">
+            <span className="text-[16px] text-neutral-400 mt-[6px]">
+              {company}
             </span>
-          ))}
+            <span className="text-[14px] text-neutral-400">
+              {startDate} - {endDate}
+            </span>
+          </div>
+          <div className="hidden md:flex flex-col gap-[28px] mt-[28px]">
+            <ExperienceCardDescription descriptionItems={descriptionItems} />
+            <ExperienceCardSkills skills={skills} />
+          </div>
         </div>
       </div>
+      <div className="md:hidden mt-[28px] flex flex-col gap-[28px]">
+        <ExperienceCardDescription descriptionItems={descriptionItems} />
+        <ExperienceCardSkills skills={skills} />
+      </div>
+    </div>
+  );
+}
+
+function ExperienceCardDescription({
+  descriptionItems,
+}: {
+  descriptionItems: string[];
+}) {
+  return (
+    <ul className="text-[16px] text-neutral-100 flex flex-col gap-[12px] list-disc pl-[40px] font-light">
+      {descriptionItems.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+}
+
+function ExperienceCardSkills({ skills }: { skills: string[] }) {
+  return (
+    <div className="flex flex-wrap gap-[12px]">
+      {skills.map((skill) => (
+        <span key={skill} className="text-[16px] text-neutral-100 font-bold">
+          {skill}
+        </span>
+      ))}
     </div>
   );
 }
